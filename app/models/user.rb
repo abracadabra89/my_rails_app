726e0e895f5f17b30d2ba12e5b9a1b67655fbd22
Anya <anya.s.litvinova@gmail.com>
2019-10-23 19:42:03 -0400

    Make a basic User model (including secure passwords)

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    before_save { self.email = email.downcase }
+    validates :name,  presence: true, length: { maximum: 50 }
+    validates :email, presence: true, length: { maximum: 255 },  uniqueness: true
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
+end
