e6b05693601c3e7bfc1e57195b8828916109158c
Anya <anya.s.litvinova@gmail.com>
2019-10-23 20:36:43 -0400

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
     before_save { self.email = email.downcase }
     validates :name,  presence: true, length: { maximum: 50 }
     validates :email, presence: true, length: { maximum: 255 },  uniqueness: true
-    has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
+    
 end
