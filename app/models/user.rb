caa2637d66d4d19b6308844c70e95105bbf045e3
Anya <anya.s.litvinova@gmail.com>
2019-10-24 00:20:20 -0400

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,18 @@
 class User < ApplicationRecord
     has_many :microposts, dependent: :destroy
+    has_many :active_relationships, class_name:  "Relationship",
+    foreign_key: "follower_id", dependent:   :destroy
+    has_many :passive_relationships, class_name:  "Relationship",
+    foreign_key: "followed_id",  dependent:   :destroy
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
     attr_accessor :remember_token, :activation_token
-  before_save   :downcase_email
-  before_create :create_activation_digest
+    before_save   :downcase_email
+    before_create :create_activation_digest
     validates :name,  presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
-                      format: { with: VALID_EMAIL_REGEX },
-                      uniqueness: true
+    format: { with: VALID_EMAIL_REGEX }, uniqueness: true
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
   
@@ -49,6 +54,28 @@ class User < ApplicationRecord
       self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
+
+    def feed
+        following_ids = "SELECT followed_id FROM relationships
+                         WHERE  follower_id = :user_id"
+        Micropost.where("user_id IN (#{following_ids})
+                         OR user_id = :user_id", user_id: id)
+      end
+
+    # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
 
       
   end
